f2ec97c536a0d978123ccb9f34e93a30a671d7f5
Steve Golden <sgolden2525@outlook.com>
2018-12-11 12:45:47 -0600

    Setup and components 12/11/18 1:00pm
    Set up create-react-app
    Removed the content/presentational elements.
    Added CalcButton functional component
    Added CalcDisplay class component
    Incorporating the CalcButton and CalcDisplay components within the App component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CalcDisplay />
+        <CalcButton buttonId="clear" buttonDesc="CA" />
+        <CalcButton buttonId="multiply" buttonDesc="X" />
+        <CalcButton buttonId="divide" buttonDesc="/" />
+        <CalcButton buttonId="one" buttonDesc="/" />
+        <CalcButton buttonId="two" buttonDesc="/" />
+        <CalcButton buttonId="three" buttonDesc="/" />
+        <CalcButton buttonId="subtract" buttonDesc="/" />
+        <CalcButton buttonId="four" buttonDesc="/" />
+        <CalcButton buttonId="five" buttonDesc="/" />
+        <CalcButton buttonId="six" buttonDesc="/" />
+        <CalcButton buttonId="add" buttonDesc="/" />
+        <CalcButton buttonId="seven" buttonDesc="/" />
+        <CalcButton buttonId="eight" buttonDesc="/" />
+        <CalcButton buttonId="nine" buttonDesc="/" />
+        <CalcButton buttonId="equals" buttonDesc="="/>
+        <CalcButton buttonId="zero" buttonDesc="0" />
+        <CalcButton buttonId="decimal" buttonDesc="." />
       </div>
     );
   }
+}
+
+class CalcDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return (
+      <React.Fragment>
+        <div class="index__buttonBorder">Display</div>
+      </React.Fragment>
+    )
+  }
+}
+
+const CalcButton = (props) => {
+  return (
+    <React.Fragment>
+      <div id={props.buttonId} class="index__buttonBorder">{props.buttonDesc}</div>
+    </React.Fragment>  
+  )
 }
 
 export default App;
