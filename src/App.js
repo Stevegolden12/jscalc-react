e0857c2db4e67b20b3c28d38cca908034fc3b58a
Steve Golden <sgolden2525@outlook.com>
2018-12-11 13:10:18 -0600

    Adding state
    Added state with results varable.

--- src/App.js
+++ src/App.js
@@ -4,25 +4,29 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      result: 'Testing',
+    }
   }
   render() {
     return (
       <div className="App">
-        <CalcDisplay />
+        <CalcDisplay disResult={this.state.result} />
         <CalcButton buttonId="clear" buttonDesc="CA" />
         <CalcButton buttonId="multiply" buttonDesc="X" />
         <CalcButton buttonId="divide" buttonDesc="/" />
-        <CalcButton buttonId="one" buttonDesc="/" />
-        <CalcButton buttonId="two" buttonDesc="/" />
-        <CalcButton buttonId="three" buttonDesc="/" />
-        <CalcButton buttonId="subtract" buttonDesc="/" />
-        <CalcButton buttonId="four" buttonDesc="/" />
-        <CalcButton buttonId="five" buttonDesc="/" />
-        <CalcButton buttonId="six" buttonDesc="/" />
-        <CalcButton buttonId="add" buttonDesc="/" />
-        <CalcButton buttonId="seven" buttonDesc="/" />
-        <CalcButton buttonId="eight" buttonDesc="/" />
-        <CalcButton buttonId="nine" buttonDesc="/" />
+        <CalcButton buttonId="one" buttonDesc="1" />
+        <CalcButton buttonId="two" buttonDesc="2" />
+        <CalcButton buttonId="three" buttonDesc="3" />
+        <CalcButton buttonId="subtract" buttonDesc="-" />
+        <CalcButton buttonId="four" buttonDesc="4" />
+        <CalcButton buttonId="five" buttonDesc="5" />
+        <CalcButton buttonId="six" buttonDesc="6" />
+        <CalcButton buttonId="add" buttonDesc="+" />
+        <CalcButton buttonId="seven" buttonDesc="7" />
+        <CalcButton buttonId="eight" buttonDesc="8" />
+        <CalcButton buttonId="nine" buttonDesc="9" />
         <CalcButton buttonId="equals" buttonDesc="="/>
         <CalcButton buttonId="zero" buttonDesc="0" />
         <CalcButton buttonId="decimal" buttonDesc="." />
@@ -38,7 +42,7 @@ class CalcDisplay extends React.Component {
   render(){
     return (
       <React.Fragment>
-        <div class="index__buttonBorder">Display</div>
+        <div class="index__displayCal">{this.props.disResult}</div>
       </React.Fragment>
     )
   }
