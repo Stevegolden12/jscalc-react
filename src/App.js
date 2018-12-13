8044b77488ab38ce5f607a1b48c9935c1b9755d3
Steve Golden <sgolden2525@outlook.com>
2018-12-13 12:21:36 -0600

    passing data from child to parent
    Figured out how to pass data from a child component to a parent component.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+/* 12/12/2018 1am Trying to pass the correct information to state.result when the CalcButton is clicked
+   Look at the stackoverflow question:
+   https://stackoverflow.com/questions/38394015/how-to-pass-data-from-child-component-to-its-parent-in-reactjs
+
+https://medium.com/@ruthmpardee/passing-data-between-react-components-103ad82ebd17
+
+https://codepen.io/et10man/pen/xXrwOQ?editors=0010
+ */
+
 import React, { Component } from 'react';
 import './App.css';
 
@@ -6,30 +15,40 @@ class App extends Component {
     super(props);
 
     this.state = {
-      result: 'Testing',
+      result: 0,
     }
+    this.handleChange = this.handleChange.bind(this);
   }
+
+
+  handleChange(setVal) {
+    // do not forget to bind getData in constructor
+    this.setState({
+      result: setVal,
+    })
+  }
+
   render() {
-    return (
+     return (
       <div className="App">
         <CalcDisplay disResult={this.state.result} />
-        <CalcButton buttonId="clear" buttonDesc="CA" />
-        <CalcButton buttonId="multiply" buttonDesc="X" />
-        <CalcButton buttonId="divide" buttonDesc="/" />
-        <CalcButton buttonId="one" buttonDesc="1" />
-        <CalcButton buttonId="two" buttonDesc="2" />
-        <CalcButton buttonId="three" buttonDesc="3" />
-        <CalcButton buttonId="subtract" buttonDesc="-" />
-        <CalcButton buttonId="four" buttonDesc="4" />
-        <CalcButton buttonId="five" buttonDesc="5" />
-        <CalcButton buttonId="six" buttonDesc="6" />
-        <CalcButton buttonId="add" buttonDesc="+" />
-        <CalcButton buttonId="seven" buttonDesc="7" />
-        <CalcButton buttonId="eight" buttonDesc="8" />
-        <CalcButton buttonId="nine" buttonDesc="9" />
-        <CalcButton buttonId="equals" buttonDesc="="/>
-        <CalcButton buttonId="zero" buttonDesc="0" />
-        <CalcButton buttonId="decimal" buttonDesc="." />
+         <CalcButton buttonId="clear" buttonDesc="CA" hChange={this.handleChange} />
+         <CalcButton buttonId="multiply" buttonDesc="X" hChange={this.handleChange} />
+         <CalcButton buttonId="divide" buttonDesc="/" hChange={this.handleChange} />
+        <CalcButton buttonId="one" buttonDesc="1" hChange={this.handleChange}/>
+        <CalcButton buttonId="two" buttonDesc="2" hChange={this.handleChange}/>
+        <CalcButton buttonId="three" buttonDesc="3"  hChange={this.handleChange}/>
+        <CalcButton buttonId="subtract" buttonDesc="-" hChange={this.handleChange}/>
+        <CalcButton buttonId="four" buttonDesc="4" hChange={this.handleChange}/>
+        <CalcButton buttonId="five" buttonDesc="5" hChange={this.handleChange}/>
+        <CalcButton buttonId="six" buttonDesc="6" hChange={this.handleChange}/>
+        <CalcButton buttonId="add" buttonDesc="+" hChange={this.handleChange}/>
+        <CalcButton buttonId="seven" buttonDesc="7" hChange={this.handleChange} />
+        <CalcButton buttonId="eight" buttonDesc="8" hChange={this.handleChange} />
+        <CalcButton buttonId="nine" buttonDesc="9" hChange={this.handleChange}/>
+        <CalcButton buttonId="equals" buttonDesc="=" hChange={this.handleChange}/>
+        <CalcButton buttonId="zero" buttonDesc="0" hChange={this.handleChange}/>
+        <CalcButton buttonId="decimal" buttonDesc="." hChange={this.handleChange}/>
       </div>
     );
   }
@@ -42,18 +61,30 @@ class CalcDisplay extends React.Component {
   render(){
     return (
       <React.Fragment>
-        <div class="index__displayCal">{this.props.disResult}</div>
+        <div className="index__displayCal">{this.props.disResult}</div>
       </React.Fragment>
     )
   }
 }
 
-const CalcButton = (props) => {
-  return (
-    <React.Fragment>
-      <div id={props.buttonId} class="index__buttonBorder">{props.buttonDesc}</div>
-    </React.Fragment>  
-  )
+class CalcButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    this.props.hChange(this.props.buttonDesc);
+  }
+
+  render() {
+      
+    return (
+      <React.Fragment>
+        <div id={this.props.buttonId} className="index__buttonBorder" onClick={this.handleChange}>{this.props.buttonDesc}</div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
